ca5f726a4e385291ff870924d9c014044d3d9b52
Justin Kearse <kjmatthew1@catamount.wcu.edu>
2020-10-25 21:34:44 -0400

    converted single-method ActionListener methods to lambdas that call the assigned methods

--- src/main/java/com/johnwesthoff/bending/ui/Verify.java
+++ src/main/java/com/johnwesthoff/bending/ui/Verify.java
@@ -47,18 +47,10 @@ public class Verify extends javax.swing.JFrame {
 
         jLabel1.setText("Code:");
 
-        username.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                usernameActionPerformed(evt);
-            }
-        });
+        username.addActionListener(event -> usernameActionPerformed(event));
 
         jButton1.setText("Verify");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
-            }
-        });
+        jButton1.addActionListener(event -> jButton1ActionPerformed(event));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
