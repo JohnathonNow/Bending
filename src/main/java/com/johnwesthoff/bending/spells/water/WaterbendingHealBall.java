b703535dd26a6a9eb02da16dd97eb63e1f9122e6
John Westhoff <johnjwesthoff@gmail.com>
2021-01-21 23:10:22 -0500

    Fixed path to image for new spell

--- src/main/java/com/johnwesthoff/bending/spells/water/WaterbendingHealBall.java
+++ src/main/java/com/johnwesthoff/bending/spells/water/WaterbendingHealBall.java
@@ -18,7 +18,7 @@ public class WaterbendingHealBall extends Waterbending {
         ID = Constants.WATERBENDING;
         subID = 6;
         try {
-            icon = (loadIcon("https://west-it.webs.com/spells/watershard.png"));
+            icon = (loadIcon("watershard.png"));
         } catch (Exception ex) {
             Logger.getLogger(Spell.class.getName()).log(Level.SEVERE, null, ex);
         }
