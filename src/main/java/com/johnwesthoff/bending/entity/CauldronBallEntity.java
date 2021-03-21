ba30041f77791d97889c1959e07de1ed59008df4
John Westhoff <johnjwesthoff@gmail.com>
2021-03-21 03:00:26 -0400

    Added cauldron spell

---
+++ src/main/java/com/johnwesthoff/bending/entity/CauldronBallEntity.java
@@ -0,0 +1,101 @@
+package com.johnwesthoff.bending.entity;
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.nio.ByteBuffer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.johnwesthoff.bending.Constants;
+import com.johnwesthoff.bending.Server;
+import com.johnwesthoff.bending.logic.World;
+import com.johnwesthoff.bending.networking.handlers.DigEvent;
+import com.johnwesthoff.bending.networking.handlers.FillEvent;
+
+/**
+ *
+ * @author John
+ */
+public class CauldronBallEntity extends Entity {
+    public int radius = 16;
+    public int gravity = 1;
+    int a1, a2, a3;
+    int s1, s2, s3;
+
+    public CauldronBallEntity(int x, int y, int hspeed, int vspeed, int ma) {
+        X = x;
+        Y = y;
+        xspeed = hspeed;
+        yspeed = vspeed;
+        maker = ma;
+        a1 = 60 + r.nextInt(120);
+        a2 = 60 + r.nextInt(120);
+        a3 = 60 + r.nextInt(120);
+        s1 = r.nextInt(360);
+        s2 = r.nextInt(360);
+        s3 = r.nextInt(360);
+    }
+
+    @Override
+    public void onDraw(Graphics G, int viewX, int viewY) {
+        if (X > viewX && X < viewX + Constants.WIDTH_INT && Y > viewY && Y < viewY + Constants.HEIGHT_INT) {
+            G.setColor(Color.DARK_GRAY);
+            G.fillArc((int) (X - radius / 2) - viewX, (int) (Y - radius / 2) - viewY, radius, radius, 0, 360);
+
+            G.setColor(Color.LIGHT_GRAY);
+            G.fillArc((int) (X - 2) - viewX, (int) (Y - 2) - viewY, 4, 4, s1, a1);
+            G.fillArc((int) (X - 4) - viewX, (int) (Y - 4) - viewY, 8, 8, s2, a2);
+            G.fillArc((int) (X - 6) - viewX, (int) (Y - 6) - viewY, 12, 12, s3, a3);
+        }
+    }
+
+    @Override
+    public void onUpdate(World apples) {
+        if (!apples.inBounds(X, Y) || apples.checkCollision(X, Y)) {
+
+            alive = false;
+        }
+        yspeed += gravity;
+
+    }
+
+    @Override
+    public void onServerUpdate(Server lol) {
+        if (lol.earth.checkCollision(X, Y)) {
+            radius = 68;
+            lol.earth.ground.fillCircleW((int) X, (int) Y, radius, Constants.STONE);
+            lol.sendMessage(FillEvent.getPacket((int) (X), (int) (Y), radius, Constants.STONE));
+            radius -= 8;
+            lol.earth.ground.clearCircle((int) X, (int) Y, radius);
+            lol.sendMessage(DigEvent.getPacket((int) (X), (int) (Y), radius));
+            alive = false;
+        }
+    }
+
+    @Override
+    public void cerealize(ByteBuffer out) {
+        try {
+            Server.putString(out, this.getClass().getName());
+            out.putInt((int) X);
+            out.putInt((int) Y);
+            out.putInt((int) xspeed);
+            out.putInt((int) yspeed);
+            out.putInt(maker);
+        } catch (Exception ex) {
+            Logger.getLogger(ExplosionEntity.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public static void reconstruct(ByteBuffer in, World world) {
+        try {
+            world.entityList.add(new JuiceBallEntity(in.getInt(), in.getInt(), in.getInt(), in.getInt(), in.getInt()));
+        } catch (Exception ex) {
+            Logger.getLogger(JuiceBallEntity.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+}
---
+++ src/main/java/com/johnwesthoff/bending/spells/chemistry/ChemistryCauldron.java
@@ -0,0 +1,66 @@
+
+package com.johnwesthoff.bending.spells.chemistry;
+
+import static com.johnwesthoff.bending.util.network.ResourceLoader.loadIcon;
+
+import java.nio.ByteBuffer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.johnwesthoff.bending.Constants;
+import com.johnwesthoff.bending.Session;
+import com.johnwesthoff.bending.entity.CauldronBallEntity;
+import com.johnwesthoff.bending.logic.World;
+import com.johnwesthoff.bending.spells.Spell;
+import com.johnwesthoff.bending.util.math.Ops;
+
+public class ChemistryCauldron extends Chemistry {
+    public ChemistryCauldron() {
+        ID = Constants.CHEMISTRY;
+        subID = 0;
+        try {
+            icon = (loadIcon("cauldron.png"));
+        } catch (Exception ex) {
+            Logger.getLogger(Spell.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @Override
+    public void getAction(Session app) {
+        X = app.world.x;
+        Y = app.world.y - World.head;
+        mx = app.world.viewX;
+        my = app.world.mouseY - app.world.viewY;
+        double direction = 360 - Ops.pointDir(app.world.x - app.world.viewX, app.world.y - World.head - app.world.viewY,
+                app.world.mouseX, app.world.mouseY);
+        mx = ((int) (Ops.lengthdir_x(12, direction)));
+        my = ((int) (Ops.lengthdir_y(12, direction)));
+        maker = ID;
+        getMessage(app.out);
+    }
+
+    @Override
+    public int getCoolDown() {
+        return (int) (Constants.FPS);
+    }
+
+    @Override
+    public String getName() {
+        return "Cauldron";
+    }
+
+    @Override
+    public String getTip() {
+        return "<html>An intermediate chemistry spell<br>High Cooldown<br>Makes a stone bubble</html>";
+    }
+
+    @Override
+    public void getPassiveAction(Session app) {
+        // throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void getActionNetwork(World world, int px, int py, int mx, int my, int pid, int eid, ByteBuffer buf) {
+        world.entityList.add(new CauldronBallEntity(px, py, mx, my, pid).setID(eid));
+    }
+}
